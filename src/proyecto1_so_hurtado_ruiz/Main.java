577d5fb675eae9c6748b111ccd3b1d8b8c82fa52
JoseHurtado03 <jose.hurtado@correo.unimet.edu.ve>
2024-10-02 16:12:42 -0400

    :fire: Inicio

---
+++ src/proyecto1_so_hurtado_ruiz/Main.java
@@ -0,0 +1,14 @@
+
+package proyecto1_so_hurtado_ruiz;
+/**
+ *
+ * @authors José Hurtado y Joseph Ruiz
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        
+        int[] storage = new int[5];     //ALMACÉN
+    
+    }
+}
