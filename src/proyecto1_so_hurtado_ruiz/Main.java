3ec2d1d5214508c4b11f15a0e6d4c5a4deee9fbf
JoseHurtado03 <jose.hurtado@correo.unimet.edu.ve>
2024-10-03 11:40:15 -0400

    🚀Mejores thread Productor

--- src/proyecto1_so_hurtado_ruiz/Main.java
+++ src/proyecto1_so_hurtado_ruiz/Main.java
@@ -8,9 +8,10 @@ public class Main {
 
     public static void main(String[] args) {
         
-        int[] storage = new int[5];     //ALMACÉN 
-        Productor motherBoard = new Productor(storage, 0, 20, 1, 2000);
-        motherBoard.run();
-        
+        int[] storage = new int[5];     //ALMACÉN   |0 motherBoard| 1 CPU | 2 RAM | 3 Fuente A | 4 GPU |
+        Productor motherBoard = new Productor(storage, 8, 0, 20, 1, 2000);
+        Productor CPU = new Productor(storage, 10, 1, 26, 1, 2000);
+        motherBoard.start();
+        CPU.start(); 
     }
 }
--- src/proyecto1_so_hurtado_ruiz/Productor.java
+++ src/proyecto1_so_hurtado_ruiz/Productor.java
@@ -10,30 +10,46 @@ import java.util.logging.Logger;
 
 public class Productor extends Thread{
     private int[] storage;                //Array del almacén
+    private int storageLimit;             //Capacidad máxima del almacén
     private int index;                    //Índice que le corresponde del array
     private int salary;                   //Monto que cobra
     private int productsQuantity;         //Cantidad de producto x que produce en...
     private int days;                     //days días. (1día = 1segundo = 1000ms)
 
     private static final Logger logger = Logger.getLogger(Productor.class.getName());
-    
-    public Productor(int[] storage, int index, int salary, int productsQuantity, int days) {
+
+    public Productor(int[] storage, int storageLimit, int index, int salary, int productsQuantity, int days) {
         this.storage = storage;
+        this.storageLimit = storageLimit;
         this.index = index;
         this.salary = salary;
         this.productsQuantity = productsQuantity;
         this.days = days;
     }
     
+    
+    
     @Override
     public void run(){
         while(true){
-            try{
-                sleep(days);
-                storage[index] += productsQuantity;
-                System.out.println("Producto añadido correctamente");
-            }catch (InterruptedException e){
-                logger.log(Level.SEVERE, "Thread interrupted", e);
+            if (storage[index] < storageLimit) {
+                try {
+                    sleep(days);
+                    storage[index] += productsQuantity;
+                    System.out.println("Producto añadido correctamente");       //Solo para debug
+                    for (int i = 0; i < storage.length; i++) {                  //Solo para debug
+                        System.out.println(storage[i]);
+                    }
+                } catch (InterruptedException e) {
+                    logger.log(Level.SEVERE, "Thread interrupted", e);
+                }
+            }else{
+                try{
+                    sleep(days);
+                    System.out.println("Almacén lleno");                        //Solo para debug
+                }catch(InterruptedException e){
+                    logger.log(Level.SEVERE, "Thread interrupted", e);
+                }
             }
         }
     }
