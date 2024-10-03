8a2eca193ad6a37c6101c1248f04c4190f3f1877
JoseHurtado03 <jose.hurtado@correo.unimet.edu.ve>
2024-10-03 12:10:58 -0400

    🚀 Más arreglos Thread Productor

--- src/proyecto1_so_hurtado_ruiz/Main.java
+++ src/proyecto1_so_hurtado_ruiz/Main.java
@@ -7,11 +7,19 @@ package proyecto1_so_hurtado_ruiz;
 public class Main {
 
     public static void main(String[] args) {
+        //EMPRESA HP
+        int[] storageHP = new int[5];     //ALMACÉN   [|0 motherBoard| 1 CPU | 2 RAM | 3 Fuente A | 4 GPU |]
+        Productor motherBoard = new Productor(storageHP, 25, 0, 20, 1, 2000);
+        Productor CPU = new Productor(storageHP, 20, 1, 26, 1, 2000);
+        Productor RAM = new Productor(storageHP, 55, 2, 40, 3, 1000);
+        Productor powerSupply = new Productor(storageHP, 35, 3, 16, 3, 1000);
+        Productor GPU = new Productor(storageHP, 10, 4, 34, 1, 3000);
         
-        int[] storage = new int[5];     //ALMACÉN   |0 motherBoard| 1 CPU | 2 RAM | 3 Fuente A | 4 GPU |
-        Productor motherBoard = new Productor(storage, 8, 0, 20, 1, 2000);
-        Productor CPU = new Productor(storage, 10, 1, 26, 1, 2000);
+        
         motherBoard.start();
-        CPU.start(); 
+        CPU.start();
+        RAM.start();
+        powerSupply.start();
+        GPU.start();
     }
 }
