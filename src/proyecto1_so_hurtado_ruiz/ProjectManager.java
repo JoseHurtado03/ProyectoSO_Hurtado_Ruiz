873cc919a0846eff774aadf0b5bd9150a2374dc7
JoseHurtado03 <jose.hurtado@correo.unimet.edu.ve>
2024-10-04 22:44:46 -0400

    👷 Iniciando el Thread Project Manager

---
+++ src/proyecto1_so_hurtado_ruiz/ProjectManager.java
@@ -0,0 +1,31 @@
+
+package proyecto1_so_hurtado_ruiz;
+
+import java.util.logging.Logger;
+
+public class ProjectManager extends Thread{
+    int daysCounter;                //Contador de los días que faltan para entregar las computadoras
+    boolean isWatchingAnime;        //Booleano que indica si el PM está viendo anime
+    int salary;                     //Salario del PM (no sé como calcularlo)
+    int discount;                   //Cantidad de salario que se le descuenta cuando lo ven viendo anime
+
+    private static final Logger logger = Logger.getLogger(Productor.class.getName());
+    
+    public ProjectManager(int daysCounter, int salary, int discount) {
+        this.daysCounter = daysCounter;
+        this.isWatchingAnime = false;
+        this.salary = salary;
+        this.discount = discount;
+    }
+    
+    @Override
+    public void run(){
+        while(true){
+            //Si el tiempo del día es menor a 667ms{
+            //    Cada 20.833ms cambia el estado de isWatchingAnime
+            //}Sino{
+            //    Actualiza el daysCounter;
+            //}
+        }
+    }
+}
